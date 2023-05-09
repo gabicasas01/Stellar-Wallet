4afa571832c9555b4be6b5e4b77a8ed01220e8ca
gabicasas01 <casas.gf7@gmail.com>
2023-05-09 14:43:20 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const PORT = 3001
+
+app.get('/', (_req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
