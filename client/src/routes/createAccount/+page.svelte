fe1780ea94ff0158e823731b3eccc8fb60866d33
gabicasas01 <casas.gf7@gmail.com>
2023-05-10 18:44:44 -0300

    Edit create Account routes

--- client/src/routes/createAccount/+page.svelte
+++ client/src/routes/createAccount/+page.svelte
@@ -1,22 +1,44 @@
 <script lang="ts">
-    import { createAccount } from "../../utils/createAccount.ts";
-  
-    const newAccount = {
-      publicKey: '',
-      secretKey: '',
-    }
-  
-    const handleCreateAccount = async () => {
-      const { publicKey, secretKey } = await createAccount();
-      newAccount.publicKey = publicKey;
-      newAccount.secretKey = secretKey;
-    }
-  
-  </script>
-  
-  <button on:click={handleCreateAccount}>Create Account</button>
-  
-  <p>Public key: {newAccount.publicKey}</p>
-  <p>Secret key: {newAccount.secretKey}</p>
+  import { createAccount } from "../../utils/createAccount.ts";
+  import AddFunds from '../../components/AddFunds.svelte';
+  import GetBalance from '../../components/GetBalance.svelte'
 
-  <a href="/addFunds">Add Funds</a>
+  let newAccount = {
+    publicKey: '',
+    secretKey: '',
+  };
+
+  let showGetBalance = false
+
+  const handleCreateAccount = async () => {
+    const { publicKey, secretKey } = await createAccount();
+    newAccount.publicKey = publicKey;
+    newAccount.secretKey = secretKey;
+  }
+  
+  const handleGetBalance = () => {
+    showGetBalance = !showGetBalance
+  }
+</script>
+
+<main>
+  <button on:click={handleCreateAccount}>Create</button>
+  {#if newAccount.publicKey.length > 1 && newAccount.secretKey.length > 1}
+    <h1>My account</h1>
+    <p>Public key: {newAccount.publicKey}</p>
+    <p>Secret key: {newAccount.secretKey}</p>
+    <AddFunds {newAccount} />
+    <button on:click={handleGetBalance}>Get Balance</button>
+    {#if showGetBalance}
+      <GetBalance {newAccount} /> 
+    {/if}
+  {/if}
+</main>
+
+<style>
+  main {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 2rem;
+  }
+</style>
