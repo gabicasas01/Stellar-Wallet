6986e3dd60fdfa4150a99c4850ec61d142ea0f9c
gabicasas01 <casas.gf7@gmail.com>
2023-05-10 15:33:46 -0300

    Add routes to the sveltekit enviroment

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,14 +1,8 @@
 <script>
-    import CreateAccountButton from './CreateAccountButton.svelte'
-    
 </script>
-
-<style>
-  
-</style>
 
 <h1>Svelte Wallet App</h1>
 
 <p>Welcome to your Svelte Wallet App!</p>
 
-<CreateAccountButton/>
+<a href="/createAccount">Create Account</a>
---
+++ client/src/routes/createAccount/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { createAccount } from "../../utils/createAccount.ts";
+  
+    const newAccount = {
+      publicKey: '',
+      secretKey: '',
+    }
+  
+    const handleCreateAccount = async () => {
+      const { publicKey, secretKey } = await createAccount();
+      newAccount.publicKey = publicKey;
+      newAccount.secretKey = secretKey;
+    }
+  
+  </script>
+  
+  <button on:click={handleCreateAccount}>Create Account</button>
+  
+  <p>Public key: {newAccount.publicKey}</p>
+  <p>Secret key: {newAccount.secretKey}</p>
+
+  <a href="/addFunds">Add Funds</a>
