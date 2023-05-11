52b37237e900e4c9bdbd917f38c2af2e3539812c
gabicasas01 <casas.gf7@gmail.com>
2023-05-11 09:51:16 -0300

    Add message for AddFunds component

--- client/src/components/AddFunds.svelte
+++ client/src/components/AddFunds.svelte
@@ -1,20 +1,34 @@
 <script lang="ts">
-    import { fundAccount } from '../utils/addFunds.ts';
+  import { fundAccount } from '../utils/addFunds.ts';
 
-    export let newAccount: {
-      publicKey: string;
-      secretKey: string;
-    };
+  export let newAccount: {
+    publicKey: string;
+    secretKey: string;
+  };
 
-    const handleAddFunds = async () => {
+  let message: string = '';
+
+  const handleAddFunds = async () => {
+    try {
+      message = 'Loading...'
       const response = await fundAccount(newAccount.publicKey, 200);
-      console.log(response);
-    };
-    </script>
-    
-    <form>
-      <button type="button" on:click={handleAddFunds}>
-        Agregar fondos
-      </button>
-    </form>
-    
+      if(response.status >= 400) throw new Error
+      message = 'Funds added successfully';
+    } catch (error) {
+      console.error('Error:', error);
+      message = 'Error adding funds';
+    }
+  };
+</script>
+
+<main>
+  <form>
+    <button type="button" on:click={handleAddFunds}>
+      Agregar fondos
+    </button>
+  </form>
+  
+  {#if message}
+    <p>{message}</p> 
+  {/if}
+</main>
