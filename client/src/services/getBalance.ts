7c9b0db2f48ed244a69a0a121d793fa5aa43e865
gabicasas01 <casas.gf7@gmail.com>
2023-05-17 14:50:40 -0300

    Import Server from stellar-sdk

--- client/src/services/getBalance.ts
+++ client/src/services/getBalance.ts
@@ -1,18 +1,17 @@
-import StellarSdk, { Horizon } from 'stellar-sdk';
+import { Server, Horizon } from 'stellar-sdk';
 
 const VITE_HORIZON_URL = import.meta.env['VITE_HORIZON_URL'];
 
 type BalanceLine = Horizon.BalanceLine;
 
-const server = new StellarSdk.Server(VITE_HORIZON_URL);
+const server = new Server(VITE_HORIZON_URL);
 
 export const fetchBalance = async (publicKey: string): Promise<string> => {
   try {
     const account = await server.loadAccount(publicKey);
     const balanceLine = account.balances.find((asset: BalanceLine) => asset.asset_type === 'native');
     if (balanceLine) {
-      const balance = balanceLine.balance;
-      return balance;
+      return balanceLine.balance;
     } else {
       throw new Error('Native balance not found.');
     }
