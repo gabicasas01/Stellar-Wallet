265779800d7297d2cc818cd8961b5717680b5bd1
gabicasas01 <casas.gf7@gmail.com>
2023-05-18 13:11:36 -0300

    Change Cypress alias

--- client/cypress/e2e/createAccount.cy.ts
+++ client/cypress/e2e/createAccount.cy.ts
@@ -2,29 +2,29 @@
 describe("My Account Component", () => {
   beforeEach(() => {
     cy.visit("/account");
-    cy.get("main h1").as("mainH1");
-    cy.get("main p").as("mainP");
-    cy.get("main button").as("mainBTN");
+    cy.get("main h1").as("mainTitle");
+    cy.get("main p").as("mainParagraph");
+    cy.get("main button").as("mainButton");
   });
 
   it("should return a pair of keys (publicKey and secretKey) that are string values", () => {
-    cy.get("@mainH1").contains("My account");
-    cy.get("@mainP").contains("Public key:");
+    cy.get("@mainTitle").contains("My account");
+    cy.get("@mainParagraph").contains("Public key:");
     cy.get("p:contains('Public key:')").invoke("text").should("be.a", "string");
     cy.get("p:contains('Secret key:')").invoke("text").should("be.a", "string");
   });
 
   it("should increase the account balance when clicking on addFunds", () => {
-    const fundsStatus: 'Funds: Loading...' | 'Funds: Added successfully' | 'Funds: Error adding funds' = 'Funds: Added successfully';
-    cy.get("@mainBTN").contains("Add funds").click();
-    cy.get("@mainP").contains(fundsStatus);
-    cy.get("p:contains('Funds:')").invoke("text").should("be.a", "string");
+    const fundsStatus: 'Status: Loading...' | 'Status: Added successfully' | 'Status: Error adding funds' = 'Status: Added successfully';
+    cy.get("@mainButton").contains("Add funds").click();
+    cy.get("@mainParagraph").contains(fundsStatus);
+    cy.get("p:contains('Status:')").invoke("text").should("be.a", "string");
   });
 
   it("should return the current balance of the account when clicking on balance", () => {
     const balanceStatus: 'Balance: Loading...' | 'Balance: Error fetching balance' = 'Balance: Error fetching balance';
-    cy.get("@mainBTN").contains("Balance").click();
-    cy.get("@mainP").contains(balanceStatus);
+    cy.get("@mainButton").contains("Balance").click();
+    cy.get("@mainParagraph").contains(balanceStatus);
     cy.get("p:contains('Balance:')").invoke("text").should("be.a", "string");
   });
   
