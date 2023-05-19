a8c73a6525fd64dd31d718b59bf64d00203c5742
gabicasas01 <casas.gf7@gmail.com>
2023-05-19 16:04:43 -0300

    Create GetOperations component

---
+++ client/src/components/GetOperations.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { getOperations } from '../services/getOperations';
+  import { account } from '../store/store.js';
+  import type { ICreateAccount, IPayment, IRecord } from '../utils/IOperations.js';
+
+  const { publicKey } = $account;
+  let records: (ICreateAccount | IPayment | IRecord)[] = [];
+  let status: string = 'Loading...';
+
+  onMount(async () => {
+    try {
+      records = await getOperations(publicKey);
+      if (records.length === 0) {
+        status = 'No transactions found';
+      }
+    } catch (error) {
+      status = 'No transactions found';
+    }
+  });
+</script>
+
+<main>
+  <h1>My operations</h1>
+  {#if records.length === 0}
+    <p>{status}</p>
+  {:else}
+    <ul>
+      {#each records as record}
+      <li>
+        <h3>{record.type}</h3>
+        {#if record.type === 'create_account'}
+          <p>Account: {record.account}</p>
+          <p>Created At: {record.created_at}</p>
+        {:else if record.type === 'payment'}
+          <p>Amount: {record.amount}</p>
+          <p>From: {record.source_account}</p>
+          <p>To: {record.to}</p>
+          <p>Created At: {record.created_at}</p>
+        {/if}
+      </li>
+      {/each}
+    </ul>
+  {/if}
+</main>
+
+<style>
+  main {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 2rem;
+  }
+</style>
