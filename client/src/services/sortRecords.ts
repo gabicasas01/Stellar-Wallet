f050d98060d291c42b6e47e54d41d29e4bd6e481
gabicasas01 <casas.gf7@gmail.com>
2023-05-22 15:36:26 -0300

    Delete unnecessary variable on sortRecords

--- client/src/services/sortRecords.ts
+++ client/src/services/sortRecords.ts
@@ -4,11 +4,9 @@ import type { ICreateAccount } from 'src/utils/ICreateAccount';
 import type { IRecord } from 'src/utils/IRecord';
 
 export const sortRecords = (operations: IOperations): (IPayment | ICreateAccount | IRecord)[] => {
-  const sortedRecords = operations.records.sort((a, b) => {
+  return operations.records.sort((a, b) => {
     const dateA = new Date(a.created_at);
     const dateB = new Date(b.created_at);
     return dateB.getTime() - dateA.getTime();
   });
-
-  return sortedRecords;
 };
